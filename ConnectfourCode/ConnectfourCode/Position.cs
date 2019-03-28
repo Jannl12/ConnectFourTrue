f110c0a27f6dc9e40fc821f2b709d854f3c53ddd
Emil Mayoh <emayoh18@student.aau.dk>
2019-03-28 14:08:40 +0100

    added position class

---
+++ ConnectfourCode/ConnectfourCode/Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectfourCode
+{
+    class Position
+    {
+        const int height = 6, width = 7;
+    }
+}
