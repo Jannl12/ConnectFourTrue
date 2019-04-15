d683173cec119af25a98366c6ade47e6595dacfd
Emil August Hvid Christensen <echri16@student.aau.dk>
2019-04-15 10:11:57 +0200

    Implimented negamax

--- ConnectfourCode/ConnectfourCode/NegaMax.cs
+++ ConnectfourCode/ConnectfourCode/NegaMax.cs
@@ -9,58 +9,55 @@ namespace ConnectfourCode
 {
     public class Negamax : BitBoard
     {
-
-        public int bestMove = 0;
-        private int thisIsMaxDepth = 5;
-        int[] testArray = { 10, 10, 10, 10 };//, 10, 10, 10, 10, 10, 10, 10 };
+        public int bestMove = 3;
+        public int thisIsMaxDepth = 9;
         int[] turnArray = { 3, 2, 4, 1, 5, 0, 6 };
-        
+        Dictionary<int, int> TranspositionTable = new Dictionary<int, int>();
+
+
+        public void ResetBestMove() 
+        {
+            bestMove = 3;
+            TranspositionTable.Clear();
+        }
 
         private const int height = 6, width = 7;       
-        public int NegaMax(BitBoard node, int alpha, int beta, int maxDepth, int color)
+        public int NegaMax(int alpha, int beta, int maxDepth, int color)
 
             //TODO: Skal med i implementeringen
         {
+            int lookuphashCode = bitGameBoard.GetHashCode();
+
             //TODO: Lav eventuelt nyt bitboard hver gang funktionen kaldes. JAN OG MAYOH
-            if (node.IsWin() || maxDepth == 0)
+            if (IsWin() || maxDepth == 0)
             {
-                //ConsoleManager.Show();
-                //Console.WriteLine("test");
-                int test =node.EvaluateBoard() * color;
-                /*using (System.IO.StreamWriter file =
-                    new System.IO.StreamWriter(@"C:\Users\jan_n\Desktop\GameTreeData.txt", true))
-                    
-                {
-                    foreach (int i in moveHistory)
-                        file.Write(i + ",");
-                    file.WriteLine(test);
-                    //file.AutoFlush = true;
-                }*/
-                    return node.EvaluateBoard() * color; // TODO: Add heuristic score JAN OG MAYOH
+                    int evalBuffer = EvaluateBoard();
+                    //TranspositionTable.Add(lookuphashCode, evalBuffer * color);
+                    return evalBuffer * color; // TODO: Add heuristic score JAN OG MAYOH
+               
             }
-            //for(int i = 6; i >= 0; i--)
-            //foreach(int i in turnArray)
-            for(int i = 0; i < 7; i++)
+
+            foreach(int i in turnArray)
             {
-                node.MakeMove(i);
-                if (node.CanPlay(i))
+                MakeMove(i);
+                if (CanPlay(i))
                 {
-                    //testArray[maxDepth] = i; 
-                    int value = -NegaMax(node, -beta, -alpha, maxDepth - 1, -color);
-                    //comment
+                    int value = -NegaMax(-beta, -alpha, maxDepth - 1, -color);
+
                     if (value >= beta)
                     {
-                        node.UndoMove();
+                        UndoMove();
                         return value;
                     }
                     if (value > alpha)
                     {
                         alpha = value;
-                        if(thisIsMaxDepth == maxDepth)
+                        if (thisIsMaxDepth == maxDepth)
                             bestMove = i;
+
                     }
-                }                
-                node.UndoMove();
+                }
+                UndoMove();
             }
             return alpha;
         }        
