81936eca49366769fd6af142bc325c0a9996cccc
jan_n <jlund12@student.aau.dk>
2019-05-14 13:38:31 +0200

    h

--- ConnectfourCode/ConnectfourCode/negamax.cs
+++ ConnectfourCode/ConnectfourCode/negamax.cs
@@ -8,52 +8,46 @@ using Microsoft.Office.Interop.Excel;
 
 namespace ConnectfourCode
 {
-    public class Negamax : ArrayGameBoard
+    public class Negamax : BitBoard
     {
 
 
 
         public int bestMove { get; set; }
-        int[] turnArray =  { 3, 2, 4, 1, 5, 0, 6 };
+        int[] turnArray = { 3, 2, 4, 1, 5, 0, 6 };
 
- 
+
         public int NegaMax(int alpha, int beta, int depth, int color, bool firstCall)
 
-            //TODO: Skal med i implementeringen
+        //TODO: Skal med i implementeringen
         {
 
             if (IsWin() || depth == 0 || IsDraw())
             {
                 int evalBuffer = EvaluateBoard();
-                return evalBuffer*color
+                return evalBuffer * color;
             }
             int value = int.MinValue;
-
-            foreach(int move in possibleMoves())
+            List<int> moves = possibleMoves();
+            foreach (int move in moves)
             {
+                MakeMove(move);
+                value = Math.Max(value, -NegaMax(-beta, -alpha, depth - 1, -color, false));
 
-                
-                if (CanPlay(i))
+                if (value >= beta)
                 {
-                    MakeMove(i);
-                    value = Math.Max(value,-NegaMax(-beta, -alpha, depth - 1, -color, false));
-
-                    if (value >= beta)
-                    {
-                        UndoMove();
-                        return value;
-                    }
-                    if (value > alpha)
-                    {
-                        alpha = value;
-                        if (firstCall)
-                            bestMove = i;
-                    }
                     UndoMove();
+                    return value;
                 }
-                
+                if (value > alpha)
+                {
+                    alpha = value;
+                    if (firstCall)
+                        bestMove = move;
+                }
+                UndoMove();
             }
             return alpha;
-        }        
+        }
     }
 }
